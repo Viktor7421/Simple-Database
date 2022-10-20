01678bc41a2f61f6a9b49c7b51528f134afc5174
Viktor7421 <victor.ascuna@utec.edu.pe>
2022-10-20 17:13:02 -0400

    feat: First view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def list_tasks(request):
+    return render(request, 'list_tasks.html')
+
+def create_tasks(request):
+    print(request.POST)
+    return redirect('/tasks/')
